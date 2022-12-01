9d80e59bd56ef8005d1b2a506ef3b734bf4af340
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-12-01 16:43:14 +0800

    Add investor api routes

--- routes/api.php
+++ routes/api.php
@@ -24,23 +24,28 @@ route::view('/accessories-index', 'accessories.index');
 route::view('/camera-index', 'camera.index');
 route::view('/operator-index', 'operator.index');
 route::view('/service-index', 'service.index');
+route::view('/investor-index', 'investor.index');
+
 
 Route::get('/accessories/all',['uses' => 'accessoriesController@getaccessoriesAll','as' => 'accessories.getaccessoriesall'] );
 Route::get('/camera/all',['uses' => 'cameraController@getcameraAll','as' => 'camera.getcameraall'] );
-
-
+Route::get('/investor/all',['uses' => 'investorController@getinvestorAll','as' => 'investor.getinvestorall'] );
 Route::get('/operator/all',['uses' => 'operatorController@getoperatorAll','as' => 'operator.getoperatorall'] );
+Route::get('/service/all',['uses' => 'serviceController@getserviceAll','as' => 'service.getserviceall'] );
+
 
 Route::resource('operator', 'operatorController');
 Route::post('operator/post/{id}','operatorController@update');
 
+Route::resource('investor', 'investorController');
+Route::post('investor/post/{id}','investorController@update');
+
 Route::resource('accessories', 'accessoriesController');
-Route::post('accessories/post/{id}','accessoriesController@update');
+Route::post('accessories/post/{id}','accessoriesController@update');    
 
 Route::resource('camera', 'cameraController');
 Route::post('camera/post/{id}','cameraController@update');
 
-Route::get('/service/all',['uses' => 'serviceController@getserviceAll','as' => 'service.getserviceall'] );
 
 Route::resource('service', 'serviceController');
 Route::post('service/post/{id}','serviceController@update');
