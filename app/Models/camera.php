16689504883a3ff09398e3263bc21293b9693bfa
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-12-01 14:04:25 +0800

    Add camera models

---
+++ app/Models/camera.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class camera extends Model
+{
+    use HasFactory;
+    public $table = "camera";
+    public $timestamps = false;
+    public $primaryKey = "camera_id";
+    protected $fillable = [
+        "model",
+        "shuttercount",
+        "quantity",
+        "costs",
+        "image_path"
+    ];
+
+    // public function services() {
+    //     return $this->hasMany('App\Models\service');
+    // }
+}
