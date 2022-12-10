7cad18c9d2057caf4b848342fc03a216b111ce23
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2022-12-10 18:51:08 +0800

    Add eager loading

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function admins() {
+        return $this->hasOne('App\Models\admin','user_id','admin_id');
+    }
 }
