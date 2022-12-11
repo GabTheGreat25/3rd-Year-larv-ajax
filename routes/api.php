7b6f64e70f79dd10e4ab7ce74be047230d6d040d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2022-12-12 00:03:44 +0800

    Add route middleware

--- routes/api.php
+++ routes/api.php
@@ -25,72 +25,88 @@ use Illuminate\Support\Facades\Route;
 //     'uses' => 'LoginController@getRegister',
 // ]);
 
-Route::redirect('/', 'login');
+Route::middleware('guest')->group(function () {
+    Route::redirect('/', 'login');
 
-Route::post('login-auth', [
+    Route::post('login-auth', [
     'uses' => 'LoginController@login',
     'as' => 'user.login',
-]);
-Route::get('login', [
+    ]);
+
+    Route::get('login', [
     'uses' => 'LoginController@getLogin',
-]);
-Route::get('logout',[
+    ]);
+    
+    Route::get('logout',[
     'uses' => 'LoginController@logout',
-]);
+    ]);
+});
 
 Route::middleware('auth:api')->group(function () {
 
-Route::view('/home', 'home');
+    Route::view('/home', 'home');
 
-Route::view('/admin-index', 'admin.index');
-Route::view('/admin-register', 'admin.register');
-Route::get('/admin/all',['uses' => 'adminController@getadminAll','as' => 'admin.getadminall'] );
-Route::resource('admin', 'adminController');
-Route::post('admin/post/{id}','adminController@update');
-Route::patch('/admin/restore/{id}', 'adminController@restore');
+    Route::middleware('role:admin')->group(function () {
+        Route::view('/admin-index', 'admin.index');
+        Route::view('/admin-register', 'admin.register');
+        Route::get('/admin/all',['uses' => 'adminController@getadminAll','as' => 'admin.getadminall'] );
+        Route::resource('admin', 'adminController');
+        Route::post('admin/post/{id}','adminController@update');
+        Route::patch('/admin/restore/{id}', 'adminController@restore');
+    });
 
-Route::view('/operator-index', 'operator.index');
-Route::view('/operator-register', 'operator.register');
-Route::get('/operator/all',['uses' => 'operatorController@getoperator','as' => 'operator.getoperatorall'] );
-Route::resource('operator', 'operatorController');
-Route::post('operator/post/{id}','operatorController@update');
-Route::patch('/operator/restore/{id}', 'operatorController@restore');
+    Route::middleware('role:operator')->group(function () {
+        Route::view('/operator-index', 'operator.index');
+        Route::view('/operator-register', 'operator.register');
+        Route::get('/operator/all',['uses' => 'operatorController@getoperator','as' => 'operator.getoperatorall'] );
+        Route::resource('operator', 'operatorController');
+        Route::post('operator/post/{id}','operatorController@update');
+        Route::patch('/operator/restore/{id}', 'operatorController@restore');
+    });
 
-route::view('/investor-index', 'investor.index');
-Route::view('/investor-register', 'investor.register');
-Route::get('/investor/all',['uses' => 'investorController@getinvestorAll','as' => 'investor.getinvestorall'] );
-Route::resource('investor', 'investorController');
-Route::post('investor/post/{id}','investorController@update');
-Route::patch('/investor/restore/{id}', 'investorController@restore');
+    Route::middleware('role:investor')->group(function () {
+        Route::view('/investor-index', 'investor.index');
+        Route::view('/investor-register', 'investor.register');
+        Route::get('/investor/all',['uses' => 'investorController@getinvestorAll','as' => 'investor.getinvestorall'] );
+        Route::resource('investor', 'investorController');
+        Route::post('investor/post/{id}','investorController@update');
+        Route::patch('/investor/restore/{id}', 'investorController@restore');
+    });
 
-route::view('/client-index', 'client.index');
-Route::view('/client-register', 'client.register');
-Route::get('/client/all',['uses' => 'clientController@getclientAll','as' => 'client.getclientall'] );
-Route::resource('client', 'clientController');
-Route::post('client/post/{id}','clientController@update');
-Route::patch('/client/restore/{id}', 'clientController@restore');
+    Route::middleware('role:investor')->group(function () {
+        Route::view('/client-index', 'client.index');
+        Route::view('/client-register', 'client.register');
+        Route::get('/client/all',['uses' => 'clientController@getclientAll','as' => 'client.getclientall'] );
+        Route::resource('client', 'clientController');
+        Route::post('client/post/{id}','clientController@update');
+        Route::patch('/client/restore/{id}', 'clientController@restore');
+    });
 
 
-route::view('/service-index', 'service.index');
-Route::view('/service-register', 'service.register');
-Route::get('/service/all',['uses' => 'serviceController@getserviceAll','as' => 'service.getserviceall'] );
-Route::resource('service', 'serviceController');
-Route::post('service/post/{id}','serviceController@update');
-Route::patch('/service/restore/{id}', 'serviceController@restore');
+    Route::middleware('role:admin,client,operator')->group(function () {
+        Route::view('/service-index', 'service.index');
+        Route::view('/service-register', 'service.register');
+        Route::get('/service/all',['uses' => 'serviceController@getserviceAll','as' => 'service.getserviceall'] );
+        Route::resource('service', 'serviceController');
+        Route::post('service/post/{id}','serviceController@update');
+        Route::patch('/service/restore/{id}', 'serviceController@restore');
+    });
 
-route::view('/camera-index', 'camera.index');
-Route::view('/camera-register', 'camera.register');
-Route::get('/camera/all',['uses' => 'cameraController@getcameraAll','as' => 'camera.getcameraall'] );
-Route::resource('camera', 'cameraController');
-Route::post('camera/post/{id}','cameraController@update');
-Route::patch('/camera/restore/{id}', 'cameraController@restore');
+    Route::middleware('role:admin,client,investor')->group(function () {
+    Route::view('/camera-index', 'camera.index');
+    Route::view('/camera-register', 'camera.register');
+    Route::get('/camera/all',['uses' => 'cameraController@getcameraAll','as' => 'camera.getcameraall'] );
+    Route::resource('camera', 'cameraController');
+    Route::post('camera/post/{id}','cameraController@update');
+    Route::patch('/camera/restore/{id}', 'cameraController@restore');
 
-route::view('/accessories-index', 'accessories.index');
-Route::view('/accessories-register', 'accessories.register');
-Route::get('/accessories/all',['uses' => 'accessoriesController@getaccessoriesAll','as' => 'accessories.getaccessoriesall'] );
-Route::resource('accessories', 'accessoriesController');
-Route::post('accessories/post/{id}','accessoriesController@update');
-Route::patch('/accessories/restore/{id}', 'accessoriesController@restore');
+    Route::view('/accessories-index', 'accessories.index');
+    Route::view('/accessories-register', 'accessories.register');
+    Route::get('/accessories/all',['uses' => 'accessoriesController@getaccessoriesAll','as' => 'accessories.getaccessoriesall'] );
+    Route::resource('accessories', 'accessoriesController');
+    Route::post('accessories/post/{id}','accessoriesController@update');
+    Route::patch('/accessories/restore/{id}', 'accessoriesController@restore');
+    });
 });
 
 
--- routes/web.php
+++ routes/web.php
@@ -41,33 +41,30 @@ Route::get('logout',[
 
 Route::view('/home', 'home');
 
-// Route::middleware('auth:api')->group(function () {
-
 Route::resource('admin', 'adminController');
-route::view('/admin-index', 'admin.index');
-route::view('/admin-register', 'admin.register');
+Route::view('/admin-index', 'admin.index');
+Route::view('/admin-register', 'admin.register');
 
 Route::resource('operator', 'operatorController');
-route::view('/operator-index', 'operator.index');
-route::view('/operator-register', 'operator.register');
+Route::view('/operator-index', 'operator.index');
+Route::view('/operator-register', 'operator.register');
 
 Route::resource('investor', 'investorController');
-route::view('/investor-index', 'investor.index');
-route::view('/investor-register', 'investor.register');
+Route::view('/investor-index', 'investor.index');
+Route::view('/investor-register', 'investor.register');
 
 Route::resource('client', 'clientController');
-route::view('/client-index', 'client.index');
-route::view('/client-register', 'client.register');
+Route::view('/client-index', 'client.index');
+Route::view('/client-register', 'client.register');
 
 Route::resource('service', 'serviceController');
-route::view('/service-index', 'service.index');
+Route::view('/service-index', 'service.index');
 
 Route::resource('camera', 'cameraController');
-route::view('/camera-index', 'camera.index');
+Route::view('/camera-index', 'camera.index');
 
 Route::resource('accessories', 'accessoriesController');
-route::view('/accessories-index', 'accessories.index');
-// });
+Route::view('/accessories-index', 'accessories.index');
 
 
 
