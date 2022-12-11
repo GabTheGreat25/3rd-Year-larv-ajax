19bb5a8e008d750c089fd4af4d0f6d507b6bf590
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2022-12-11 20:14:03 +0800

    Fix index blade of investor

--- resources/views/investor/index.blade.php
+++ resources/views/investor/index.blade.php
@@ -26,9 +26,10 @@
         <thead>
             <tr>
                 <th>Investor ID</th>
-                <th>name</th>
-                <th>age</th>
-                <th>contact_number</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Contact Number</th>
+                <th>Email</th>
                 <th>Image</th>
                 <th>Actions</th>
             </tr>
@@ -43,7 +44,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Create new investor</h4>
+                <h4 class="modal-title">Update Investor Information</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div style="padding: 0 2rem;">
@@ -56,24 +57,22 @@
                         <input type="text" class="form-control" id="full_name" name="full_name">
                     </div>
                     <div class="form-group">
-                        <label for="age" class="control-label">age</label>
+                        <label for="age" class="control-label">Age</label>
                         <input type="number" class="form-control" id="age" name="age" min="1" max="10">
                     </div>
                     <div class="form-group">
-                        <label for="contact_number" class="control-label">contact_number</label>
+                        <label for="contact_number" class="control-label">Contact Number</label>
                         <input type="text" class="form-control" id="contact_number" name="contact_number">
                     </div>
                     <div class="form-group">
-                        <label for="uploads" class="control-label">investor Image</label>
+                        <label for="uploads" class="control-label">Investor Image</label>
                         <input type="file" class="form-control" id="uploads" name="uploads">
                     </div>
                 </form>
             </div>
 
-
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
-                <button id="investorSubmit" type="submit" class="btn btn-primary">Save</button>
                 <button id="investorUpdate" type="submit" class="btn btn-info">Update</button>
             </div>
         </div>
