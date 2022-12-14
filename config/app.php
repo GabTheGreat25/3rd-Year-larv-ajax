90bebd2787b508508ece3802429305439f476e9f
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2022-12-14 20:20:32 +0800

    Add profanity custom language

--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
@@ -175,7 +174,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-
+        Askedio\Laravel5ProfanityFilter\Providers\ProfanityFilterServiceProvider::class,
     ],
 
     /*
