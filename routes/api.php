d2c8e22364f2187843bb76e7304d8b64675d1863
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2022-12-14 20:20:39 +0800

    Fix register bug

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,15 @@ Route::middleware('guest')->group(function () {
     Route::get('logout',[
     'uses' => 'LoginController@logout',
     ]);
+    
+    Route::view('/admin-register', 'admin.register');
+    Route::resource('admin', 'adminController')->except(['index','edit','update','destroy','restore']);
+    Route::view('/operator-register', 'operator.register');
+    Route::resource('operator', 'operatorController')->except(['index','edit','update','destroy','restore']);
+    Route::view('/investor-register', 'investor.register');
+    Route::resource('investor', 'investorController')->except(['index','edit','update','destroy','restore']);
+    Route::view('/client-register', 'client.register');
+    Route::resource('client', 'clientController')->except(['index','edit','update','destroy','restore']);
 });
 
 Route::middleware('auth:api')->group(function () {
@@ -48,40 +57,35 @@ Route::middleware('auth:api')->group(function () {
 
     Route::middleware('role:admin')->group(function () {
         Route::view('/admin-index', 'admin.index');
-        Route::view('/admin-register', 'admin.register');
+        Route::resource('admin', 'adminController')->except(['create', 'store']);
         Route::get('/admin/all',['uses' => 'adminController@getadminAll','as' => 'admin.getadminall'] );
-        Route::resource('admin', 'adminController');
         Route::post('admin/post/{id}','adminController@update');
         Route::patch('/admin/restore/{id}', 'adminController@restore');
     });
 
     Route::middleware('role:operator')->group(function () {
         Route::view('/operator-index', 'operator.index');
-        Route::view('/operator-register', 'operator.register');
+        Route::resource('operator', 'operatorController')->except(['create', 'store']);
         Route::get('/operator/all',['uses' => 'operatorController@getoperator','as' => 'operator.getoperatorall'] );
-        Route::resource('operator', 'operatorController');
         Route::post('operator/post/{id}','operatorController@update');
         Route::patch('/operator/restore/{id}', 'operatorController@restore');
     });
 
     Route::middleware('role:investor')->group(function () {
         Route::view('/investor-index', 'investor.index');
-        Route::view('/investor-register', 'investor.register');
+        Route::resource('investor', 'investorController')->except(['create', 'store']);
         Route::get('/investor/all',['uses' => 'investorController@getinvestorAll','as' => 'investor.getinvestorall'] );
-        Route::resource('investor', 'investorController');
         Route::post('investor/post/{id}','investorController@update');
         Route::patch('/investor/restore/{id}', 'investorController@restore');
     });
 
-    Route::middleware('role:investor')->group(function () {
+    Route::middleware('role:client')->group(function () {
         Route::view('/client-index', 'client.index');
-        Route::view('/client-register', 'client.register');
+        Route::resource('client', 'clientController')->except(['create', 'store']);
         Route::get('/client/all',['uses' => 'clientController@getclientAll','as' => 'client.getclientall'] );
-        Route::resource('client', 'clientController');
         Route::post('client/post/{id}','clientController@update');
         Route::patch('/client/restore/{id}', 'clientController@restore');
     });
-
 
     Route::middleware('role:admin,client,operator')->group(function () {
         Route::view('/service-index', 'service.index');
