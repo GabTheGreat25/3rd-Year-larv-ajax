0d3e117af3d6fd92361f1156a6c9a213ba621261
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2022-12-20 22:53:47 +0800

    Add login js

---
+++ public/js/login.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $("#loginbtn").on("click", function (e) {
+        e.preventDefault();
+        var data = $("#loginform")[0];
+        console.log(data);
+        let formData = new FormData(data);
+        console.log(formData);
+        for (var pair of formData.entries()) {
+            console.log(pair[0] + "," + pair[1]);
+        }
+        $.ajax({
+            type: "POST",
+            url: "/api/login-auth",
+            data: formData,
+            contentType: false,
+            processData: false,
+            // beforeSend: function (header) {
+            //     /* Authorization header */
+            //     header.setRequestHeader(
+            //         "Authorization",
+            //         "Bearer " + localStorage.getItem("token")
+            //     );
+            // },
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+            },
+            dataType: "json",
+            success: function () {
+                window.location = "/home";
+            },
+            error: function (error) {
+                console.log(error);
+            },
+        });
+    });
+});
