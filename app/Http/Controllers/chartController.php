d184bbc036a133ff5755f474caa9a4f6b37c5542
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2022-12-27 23:47:30 +0800

    Add 2 charts

--- app/Http/Controllers/chartController.php
+++ app/Http/Controllers/chartController.php
@@ -22,4 +22,32 @@ class chartController extends Controller
     public function getOperatorChart(){
         return view('charts.index');
     }
+
+    public function salesChart() {
+        $sales = DB::table('camera as c')
+                    ->join('camera_transaction_line as cl', 'c.camera_id', '=', 'cl.camera_id')
+                    ->join('transaction as tr', 'cl.transaction_id', '=', 'tr.transaction_id')
+                    ->select(DB::raw('monthname(tr.date_of_rent) as month, sum(cl.quantity * c.costs) as total'))
+                    ->groupBy('tr.date_of_rent')
+                    ->pluck('total','month')
+                    ->all();
+        $labels = (array_keys($sales));
+    
+        $data= array_values($sales);
+        return response()->json(array('data' => $data, 'labels' => $labels));
+    }
+
+    public function accChart() {
+        $sales = DB::table('accessories as a')
+                    ->join('accessories_transaction_line as al', 'a.accessories_id', '=', 'al.accessories_id')
+                    ->join('transaction as tr', 'al.transaction_id', '=', 'tr.transaction_id')
+                    ->select(DB::raw('a.description as items, sum(al.quantity) as total'))
+                    ->groupBy('a.description')
+                    ->pluck('total','items')
+                    ->all();
+        $labels = (array_keys($sales));
+    
+        $data= array_values($sales);
+        return response()->json(array('data' => $data, 'labels' => $labels));
+    }
 }
