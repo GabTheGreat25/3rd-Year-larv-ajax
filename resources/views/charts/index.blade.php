ff94a4b6ce5086ace121dfcfb117267815dd59da
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-01-03 17:16:21 +0800

    Update charts blade views interface

--- resources/views/charts/index.blade.php
+++ resources/views/charts/index.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.base')
 @section('body')
+<body class="w-40 h-40 bg-cover" style="background-image: url('https://wallpapers.com/images/file/canon-dslr-zoom-photography-lenses-evtrr0i1swpetu82.jpg');">
+ 
 <style>
     .container-fluid {
         display: grid;
         grid-template-columns: 1fr 1fr 1fr;
     }
 </style>
-
+<form action="" class="grid grid-cols-2 gap-12 mt-14 lg:mt-28">
 <div class="container-fluid">
     <div class="f">
         <canvas id="operatorChart"></canvas>
@@ -20,6 +22,7 @@
         <canvas id="accChart"></canvas>
     </div>
 </div>
-
+</form>
 </div>
+</body>
 @endsection
